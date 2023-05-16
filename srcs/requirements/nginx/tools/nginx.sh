ac95f9d76034ca4505bd10f43312986e5a7a14e8
root <root@DESKTOP-UPUMT3G>
2023-05-16 14:27:16 +0100

    ok

--- srcs/requirements/nginx/tools/nginx.sh
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -1,2 +1,6 @@
-echo aaa >> /etc/hosts;
+openssl genrsa -out ted-dafi.42.fr.key 2048
+openssl req -new -key ted-dafi.42.fr.key -out ted-dafi.42.fr.csr \
+-subj "/C=MO/ST=QQ/L=BenGuerir/O=UnknowCompany, Inc./OU=IT/CN=ted-dafi.42.fr"
+mkdir /openssl
+mv ted-dafi.42.fr.csr /openssl/
 nginx -g "daemon off;"
