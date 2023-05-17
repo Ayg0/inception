4eeedd761fe1877754886acc3ce6d202388a2b95
root <root@DESKTOP-UPUMT3G>
2023-05-17 12:02:18 +0100

    nginx tls 443

--- srcs/requirements/nginx/tools/nginx.sh
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -1,6 +1,7 @@
-openssl genrsa -out ted-dafi.42.fr.key 2048
-openssl req -new -key ted-dafi.42.fr.key -out ted-dafi.42.fr.csr \
--subj "/C=MO/ST=QQ/L=BenGuerir/O=UnknowCompany, Inc./OU=IT/CN=ted-dafi.42.fr"
 mkdir /openssl
-mv ted-dafi.42.fr.csr /openssl/
+openssl req -x509 -nodes -days 365 -subj \
+            "/C=MO/ST=QQ/L=BenGuerir/O=UnknowCompany, Inc./OU=IT/CN=ted-dafi.42.fr" \
+            -addext "subjectAltName=DNS:ted-dafi.42.fr" \
+            -newkey rsa:2048 -keyout /openssl/ssl_prv.key \
+            -out /openssl/ted-dafi.42.fr.crt;
 nginx -g "daemon off;"
