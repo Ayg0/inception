f0c1f8bf834359bab72a81f00290904264e6d1b8
Ayg0 <ted-dafi@student.1337.com>
2023-05-18 17:12:11 +0100

    mariadb fix

--- srcs/requirements/mariadb/conf/conf.sh
+++ srcs/requirements/mariadb/conf/conf.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 
-# mkdir -p /run/mysqld
-# chown mysql:mysql /run/mysqld
+/etc/init.d/mysql start
 
-# /etc/init.d/mysql start
+mysql -u mysql -e "CREATE DATABASE WordPress;"
 
-# mysql -u mysql -e "CREATE DATABASE WordPress;"
-
-# /etc/init.d/mysql stop
+/etc/init.d/mysql stop
 
 # mysqld --bind-address=0.0.0.0
-# mysqld --user=root
-
-mkdir -p /run/mysqld/
-chown mysql:mysql /run/mysqld
-
-service mysql start
-mysql <<< "create database if not exists wow;"
-service mysql stop
-
-mysqld --bind-address=0.0.0.0
+mysqld --user=root
